53ac5325d124547636db2b6fad7421d7fe4b304d
Judy <judychue@pursuit.org>
2023-03-20 14:19:25 -0400

    lab-forms-with-passed

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log("Code your solution!")
+const list = document.querySelector("form");
+list.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+const ul = document.querySelector("ul")
+const listText = document.getElementById("list").value;
+    if (listText) {
+    const newLi = document.createElement("li")
+    newLi.textContent = listText
+   document.body.append(newLi)
+    newLi.addEventListener("click", (event) => {
+        newLi.style.textDecoration = "line-through"
+    })
+} else document.querySelector("p").textContent = "Error! To-Do Cannot Be Empty"
+})
